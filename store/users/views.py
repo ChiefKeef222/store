8cb1c22a436b180930c0209d3fb5cba2d8cfc8d5
Arhat Amanzhol <aarhat144@gmail.com>
2025-03-27 15:40:55 +0500

    first commit

---
+++ store/users/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.contrib import auth
+from django.urls import reverse
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                form.add_error(None, "Неверный логин или пароль")  # Сообщение об ошибке
+    else:
+        form = UserLoginForm()
+    
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else: 
+        form = UserRegistrationForm()
+    context = {'form' : form}
+    return render(request, 'users/registration.html', context)
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance= request.user, data = request.POST, files=request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            if 'image' in request.FILES:  # Проверяем, есть ли файл
+                user.image = request.FILES['image']  # Принудительно добавляем
+            user.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', context)
+
+
