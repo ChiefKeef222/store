ae0a87b33de63af54478fb0195ffca0701ede9da
Arhat Amanzhol <aarhat144@gmail.com>
2025-04-04 01:25:41 +0500

    updated django admin

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'category' ,'description', ('price', 'quantity'), 'image')
+    search_fields = ('name', )
+    ordering = ('name', )
+    
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity')
+    readonly_fields = ('created_timestamp', )
+    extra = 1
--- store/products/models.py
+++ store/products/models.py
@@ -5,6 +5,10 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=256, unique=True)
     description = models.TextField(null=True, blank=True)
     
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
     def __str__(self):
         return self.name
     
@@ -15,6 +19,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
     
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
